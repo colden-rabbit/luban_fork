60f5e4429f70140fbe7d3605cda74ba7ff2074e4
colden-rabbit <2251633709@qq.com>
2025-01-17 14:33:26 +0800

    数组长度校验，增加对指定长度的校验

--- src/Luban.DataValidator.Builtin/Collection/SizeValidator.cs
+++ src/Luban.DataValidator.Builtin/Collection/SizeValidator.cs
@@ -12,33 +12,41 @@ public class SizeValidator : DataValidatorBase
     private static readonly NLog.Logger s_logger = NLog.LogManager.GetCurrentClassLogger();
 
     private LongRange _range;
+    private long[] _sizeArray;
 
     private Func<DType, long> _sizeGetter;
 
     public SizeValidator()
     {
-
     }
 
     public override void Compile(DefField field, TType type)
     {
-        _range = new LongRange(Args);
+        if (Args.Contains("|"))
+        {
+            _sizeArray = Args.Trim().Split("|").Select(long.Parse).ToArray();
+        }
+        else
+        {
+            _range = new LongRange(Args);
+        }
+
         _sizeGetter = type switch
         {
-            TList => d => ((DList)d).Datas.Count,
-            TSet => d => ((DSet)d).Datas.Count,
-            TMap => d => ((DMap)d).Datas.Count,
+            TList  => d => ((DList)d).Datas.Count,
+            TSet   => d => ((DSet)d).Datas.Count,
+            TMap   => d => ((DMap)d).Datas.Count,
             TArray => d => ((DArray)d).Datas.Count,
-            _ => throw new Exception($"type:{type} field:{field} not support size validator"),
+            _      => throw new Exception($"type:{type} field:{field} not support size validator"),
         };
     }
 
     public override void Validate(DataValidatorContext ctx, TType type, DType data)
     {
         long size = _sizeGetter(data);
-        if (!_range.CheckInRange(size))
+        if ((_sizeArray != null && !_sizeArray.Contains(size)) || (_range != null && !_range.CheckInRange(size)))
         {
-            s_logger.Error("记录 {}:{} (来自文件:{}) size:{},但要求为 {} ", DataValidatorContext.CurrentRecordPath, data, Source, size, _range.RawStr);
+            s_logger.Error("记录 {}:{} (来自文件:{}) size:{},但要求为 {} ", DataValidatorContext.CurrentRecordPath, data, Source, size, Args.Trim());
             GenerationContext.Current.LogValidatorFail(this);
         }
     }
